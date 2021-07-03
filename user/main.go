4aa5dd8d6cd32777c35b35701b8e32549e3376d0
luojie <qq603350683@gmail.com>
2021-07-03 22:30:10 +0800

    udpate main not used value

--- user/main.go
+++ user/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	ConnectMysql()
 
@@ -12,6 +14,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println(b)
 
 	_, err = AddUserCache()
 	if err != nil {
